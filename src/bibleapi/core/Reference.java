fc3d0875d6a884500b8bf2e653bd4ad7104378a2
Patrice De Saint Steban <patou@users.noreply.github.com>
2010-04-29 14:01:29 +0000

    Reference classe => parse la chaine de référence  Moteur de recherche avec Compass Début de l'import de fichier

---
+++ src/bibleapi/core/Reference.java
@@ -0,0 +1,115 @@
+package bibleapi.core;
+
+
+
+public class Reference {
+	public final static String VERSION_SEPARATOR = ":";
+	public final static String BOOK_SEPARATOR = ",";
+	public final static String CHAPTER_SEPARATOR = ",";
+	/**
+	 * Version de la bible
+	 */
+	private String version = "";
+	
+	/**
+	 * Livre de la bible
+	 */
+	private String book = "";
+	
+	/**
+	 * Chapitre du verset
+	 */
+	private Integer chapter = null;
+	
+	/**
+	 * Numéro du verset
+	 */
+	private Integer verset = null;
+	
+	public Reference() {
+	}
+	
+	public Reference(String reference) {
+		parse(reference);
+	}
+
+	public Reference(String version, String book, Integer chapter, Integer verset) {
+		this.version = version;
+		this.book = book;
+		this.chapter = chapter;
+		this.verset = verset;
+	}
+	
+	/**
+	 * Parse la chaine de caractère Référence Doit être au bon format
+	 * @param reference
+	 */
+	public void parse(String reference) {
+		int begin = 0;
+		int end = 0;
+		if (reference.indexOf(VERSION_SEPARATOR) > 0) {
+			end = reference.indexOf(VERSION_SEPARATOR);
+			version = reference.substring(begin, end);
+			begin = end + 1;
+		}
+		if (reference.indexOf(BOOK_SEPARATOR, begin) > 0) {
+			end = reference.indexOf(BOOK_SEPARATOR);
+			book = reference.substring(begin, end);
+			begin = end + 1;
+		}
+		if (reference.indexOf(CHAPTER_SEPARATOR, begin) > 0) {
+			end = reference.indexOf(CHAPTER_SEPARATOR, begin);
+			String chp = reference.substring(begin, end);
+			chapter = Integer.decode(chp);
+			begin = end + 1;
+		}
+		if (reference.length() > begin) {
+			String v = reference.substring(begin);
+			verset = Integer.decode(v);
+		}
+	}
+	
+	public String toString() {
+		return version + VERSION_SEPARATOR + book + BOOK_SEPARATOR + chapter + CHAPTER_SEPARATOR + verset;
+	}
+	
+	/**
+	 * Retourne la référence textuelle
+	 * @return
+	 */
+	public String getReference() {
+		return toString();
+	}
+
+	public String getBook() {
+		return book;
+	}
+
+	public void setBook(String book) {
+		this.book = book;
+	}
+
+	public Integer getChapter() {
+		return chapter;
+	}
+
+	public void setChapter(Integer chapter) {
+		this.chapter = chapter;
+	}
+
+	public Integer getVerset() {
+		return verset;
+	}
+
+	public void setVerset(Integer verse) {
+		this.verset = verse;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+}
--- src/bibleapi/tools/PMF.java
+++ src/bibleapi/tools/PMF.java
@@ -3,15 +3,41 @@ package bibleapi.tools;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
+import org.compass.core.Compass;
+import org.compass.core.config.CompassConfiguration;
+import org.compass.core.config.CompassEnvironment;
+import org.compass.gps.CompassGps;
+import org.compass.gps.device.jdo.JdoGpsDevice;
+import org.compass.gps.impl.SingleCompassGps;
+
 /** 
  * Gestion de persistance pour le DataStore google
  */
 public final class PMF {
     private static final PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory("transactions-optional");
     
+    private static final Compass compass;
+    private static final CompassGps compassGps;
+    
+    static {
+    	compass = new CompassConfiguration().setConnection("gae://index")
+    				.setSetting(CompassEnvironment.ExecutorManager.EXECUTOR_MANAGER_TYPE, "disabled")
+    				.addScan("bibleapi.core")
+    				.buildCompass();
+    	compassGps = new SingleCompassGps(compass);
+    	compassGps.addGpsDevice(new JdoGpsDevice("appengine", pmfInstance));
+    	compassGps.start();
+    	
+    	compassGps.index();
+    }
+    
     private PMF() {}
 
     public static PersistenceManagerFactory get() {
         return pmfInstance;
+    }
+    
+    public static Compass getCompass() {
+    	return compass;
     }
 }
